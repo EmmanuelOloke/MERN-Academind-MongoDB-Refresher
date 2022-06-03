f64b64346dbc9cca003500f9b84c5f449e82561d
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-06-04 00:26:53 +0100

    downloaded mongodb npm manager and setup mongo.js middleware

---
+++ mongo.js
@@ -0,0 +1,12 @@
+const MongoClient = require('mongodb').MongoClient; // MongoClient is class that allows us to make connection with  MongoDB
+
+const createProduct = async(req, res, next) => { // When you're working with databases, the tasks can take a while, that's why it is appropriate to use async functions
+
+};
+
+const getProducts = async(req, res, next) => {
+
+};
+
+exports.createProduct = createProduct;
+exports.getProducts = getProducts;
