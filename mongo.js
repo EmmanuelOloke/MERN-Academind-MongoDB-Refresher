d47b84ec2c90850f005c3bff614d0c3fb210736c
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-06-04 00:35:58 +0100

    installed dotenv to manage db url environment variable

--- mongo.js
+++ mongo.js
@@ -1,5 +1,7 @@
 const MongoClient = require('mongodb').MongoClient; // MongoClient is class that allows us to make connection with  MongoDB
 
+const URL = process.env.DB_URL;
+
 const createProduct = async(req, res, next) => { // When you're working with databases, the tasks can take a while, that's why it is appropriate to use async functions
 
 };
