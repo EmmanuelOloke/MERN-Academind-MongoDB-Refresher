792da94e4d1fe201fab42706e5d806f71adb46bb
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-06-04 15:41:04 +0100

    createProduct middleware

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const mongoPractice = require('./mongo');
+
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.post('/products');
+app.post('/products', mongoPractice.createProduct);
 
 app.get('/products');
 
--- mongo.js
+++ mongo.js
@@ -1,8 +1,25 @@
 const MongoClient = require('mongodb').MongoClient; // MongoClient is class that allows us to make connection with  MongoDB
 
-const URL = process.env.DB_URL;
+const URI = process.env.DB_URI;
 
 const createProduct = async(req, res, next) => { // When you're working with databases, the tasks can take a while, that's why it is appropriate to use async functions
+    const newProduct = {
+        name: req.body.name,
+        price: req.body.price
+    };
+
+    const client = new MongoClient(URI); // This line just tells the mongodb/client which server we want to connnect to. Initiating the connection doesn't take place here.
+
+    try {
+        await client.connect(); // This is the line that establishes the connection to the mongodb server.
+        const db = client.db(); // db() takes the connection string provided in const URL and gives access to the db.
+        const result = await db.collection('products').insertOne(newProduct); // If the collection doesn't exist in the db, a new collection is created and if it exists a new document is added to the collection.
+    } catch (error) {
+        return res.status(422).json({ message: 'Could not store data.' });
+    };
+    res.json(newProduct);
+
+    client.close();
 
 };
 
