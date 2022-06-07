2470c0c9a8b977302ee99de35cfac962c08ed590
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-06-07 23:38:49 +0100

    added comment to explain why connecting to the server is an asynchronous task

--- mongo.js
+++ mongo.js
@@ -11,7 +11,7 @@ const createProduct = async(req, res, next) => { // When you're working with dat
     const client = new MongoClient(URI); // This line just tells the mongodb/client which server we want to connnect to. Initiating the connection doesn't take place here.
 
     try { // Using a try/catch block because connecting to the server/database can fail. And in case of a failure, we want to catch any error that might have occured in the process
-        await client.connect(); // This is the line that establishes the connection to the mongodb server.
+        await client.connect(); // This is the line that establishes the connection to the mongodb server. An asynchronous task because connecting to the server is a task that can take a few moment.
         const db = client.db(); // db() takes the connection string provided in const URL and gives access to the db.
         const result = await db.collection('products').insertOne(newProduct); // If the collection doesn't exist in the db, a new collection is created and if it exists a new document is added to the collection.
     } catch (error) {
