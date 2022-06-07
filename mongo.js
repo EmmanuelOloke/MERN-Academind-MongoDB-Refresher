b1936c3ba9953bfa900b52cb974e38e818729bb7
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-06-07 23:57:09 +0100

    create product refactored with comments

--- mongo.js
+++ mongo.js
@@ -2,6 +2,7 @@ const MongoClient = require('mongodb').MongoClient; // MongoClient is class that
 
 const URI = process.env.DB_URI;
 
+// Creating a Document on MongoDB
 const createProduct = async(req, res, next) => { // When you're working with databases, the tasks can take a while, that's why it is appropriate to use async functions
     const newProduct = {
         name: req.body.name,
@@ -12,14 +13,14 @@ const createProduct = async(req, res, next) => { // When you're working with dat
 
     try { // Using a try/catch block because connecting to the server/database can fail. And in case of a failure, we want to catch any error that might have occured in the process
         await client.connect(); // This is the line that establishes the connection to the mongodb server. An asynchronous task because connecting to the server is a task that can take a few moment.
-        const db = client.db(); // db() takes the connection string provided in const URL and gives access to the db.
-        const result = await db.collection('products').insertOne(newProduct); // If the collection doesn't exist in the db, a new collection is created and if it exists a new document is added to the collection.
+        const db = client.db(); // db() takes the connection string provided in const URL and gives access to the db. Accessing a specific database on the server.
+        const result = await db.collection('products').insertOne(newProduct); // Here we want to access a collection in the db. Provide the name of the collection as an argument ("products"). If the collection doesn't exist in the db, a new collection is created and if it exists a new document is added to the collection.
     } catch (error) {
         return res.status(422).json({ message: 'Could not store data' });
     };
     res.json(newProduct);
 
-    client.close();
+    client.close(); // We must manually close a connection, because once a connection is opened it will remain open until we manually close it.
 
 };
 
