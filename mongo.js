456e922218b0dd4825a5bf24e8aa60d5ae8f7cf9
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-06-08 00:34:57 +0100

    implemented db query to get existing collection (products)

--- mongo.js
+++ mongo.js
@@ -1,4 +1,4 @@
-const MongoClient = require('mongodb').MongoClient; // MongoClient is class that allows us to make connection with  MongoDB
+const MongoClient = require('mongodb').MongoClient; // MongoClient is class that allows us to make connection with MongoDB
 
 const URI = process.env.DB_URI;
 
@@ -18,14 +18,28 @@ const createProduct = async(req, res, next) => { // When you're working with dat
     } catch (error) {
         return res.status(422).json({ message: 'Could not store data' });
     };
-    res.json(newProduct);
 
     client.close(); // We must manually close a connection, because once a connection is opened it will remain open until we manually close it.
 
+    res.json(newProduct);
 };
 
 const getProducts = async(req, res, next) => {
+    const client = new MongoClient(URI);
 
+    let products;
+
+    try {
+        await client.connect();
+        const db = client.db();
+        products = await db.collection('products').find().toArray(); // The find() method in mongoDB returns a cursor. A cursor is a pointer to the result of the query and it can be iterated over to see all the results. toArray() allows us to get the document in an array.
+    } catch (error) {
+        return res.status(422).json({ message: 'Could not retrieve products' });
+    };
+
+    client.close(); // We must manually close a connection, because once a connection is opened it
+
+    res.json(products);
 };
 
 exports.createProduct = createProduct;
