c12eddf1e4926356b780618a9a9827a505a27ef7
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-06-08 02:01:41 +0100

    connecting mongoose to the database through .env

--- mongoose.js
+++ mongoose.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 
 const Product = require('./models/products');
 
+mongoose.connect(process.env.DB_URI);
+
 const createProduct = async(req, res, next) => {
     const createdProduct = new Product({
         name: req.body.name,
